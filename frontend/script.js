583cc911c9aa0fe478a0c73c7a9831811eb01cee
ApothecaryOfLight <ChangerOfWays@gmail.com>
2021-02-20 20:20:24 +0000

    Restyled CSS interface. Added fake data for news topics.

--- frontend/script.js
+++ frontend/script.js
@@ -4,6 +4,7 @@ console.log( "Yipee!" );
 
 window.addEventListener( 'load', (event)=> {
   render_timeline( fake_data.oinks );
+  render_whats_happening( fake_data.news );
 });
 
 
@@ -55,7 +56,51 @@ let fake_data = {
     }
   ],
   news : [
-
+    {
+      topic: "Politics",
+      headline: "Joe Manchin is Sexist",
+      message: "Who knew!? I know no other Democrat can get elected in West Virignia, but have we considered getting rid of West Virginia?"
+    },
+    {
+      topic: "Sports",
+      headline: "Organized Sports: Worse Than Organized Crime?",
+      message: "Why can\'t y\'all just play pick up games? Why does it have to consume society like a disease?"
+    },
+    {
+      topic: "Politics",
+      headline: "Cruz Abandons Dog",
+      message: "Polls show his voters care more about that than him leaving them all to freeze."
+    },
+    {
+      topic: "Politics",
+      headline: "Low-Info Voters Upset Biden Not Using Doctor Strange Powers",
+      message: "Why hasn\'t he fixed everything yet?"
+    },
+    {
+      topic: "Science",
+      headline: "Another Rover on Mars",
+      message: "Nerds Argue About GNU vs Linux"
+    },
+    {
+      topic: "Entertainment",
+      headline: "DC Movies Continue to Suck",
+      message: "Watch Wandavision! Hahnaissance time!"
+    },
+    {
+      topic: "Crime",
+      headline: "Cocaine Cocoa-Puffs Intercepted",
+      message: "Who? Why Cincinnati? Perhaps most perplexingly: how!?"
+    },
+    {
+      topic: "Politics",
+      headline: "US Right Wingers Continue to Confuse Freedom with Whatever-The-Hell-I-Want",
+      message: "Performative anti-mask and other pro-plague behaviors continue to endanger world at large as US variants surge. Yet again, this reporter must ask: why is my country so selfish and stupid?"
+    },
+    {
+      topic: "Entertainment",
+      headline: "Judas and the Black Messiah",
+      message: "Actually happened almost exactly as depicted. Compare with West Wing and other Sorkin ventures, which have absolutely no relation to reality. Discuss."
+    }
   ]
 }
 
@@ -83,4 +128,18 @@ function render_timeline( inTimeline ) {
 
   const main_container = document.getElementById("main_container");
   main_container.innerHTML = div;
+}
+
+function render_whats_happening( news ) {
+  let dom = "<div class=\"news_items_title\">What\'s Happening</div>";
+  for( const news_item in news ) {
+    dom += "<div class=\"news_item_container\">" +
+      "<div class=\"news_item_topic\">" + news[news_item].topic + "</div>" +
+      "<div class=\"news_item_headline\">" + news[news_item].headline + "</div>" +
+      "<div class=\"news_item_message\">" + news[news_item].message + "</div>" +
+    "</div>"
+  }
+
+  const opt_container = document.getElementById("news_items_container");
+  opt_container.innerHTML = dom;
 }
